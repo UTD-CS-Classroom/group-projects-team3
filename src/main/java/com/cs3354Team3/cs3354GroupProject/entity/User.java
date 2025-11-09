3d46d1af342a5c5047d276436401881e10a880a7
KentonLe <kxl220030@gmail.com>
2025-11-08 21:25:45 -0600

    Added constructor for User class

--- src/main/java/com/cs3354Team3/cs3354GroupProject/entity/User.java
+++ src/main/java/com/cs3354Team3/cs3354GroupProject/entity/User.java
@@ -11,10 +11,17 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String email;
     private String password;
 
     @Enumerated(EnumType.STRING)
     private Role role;
+    
+    public User(Long id, String email, String password, Role role)
+    {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
 }
